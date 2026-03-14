891aef25e0e466df08d9644cde203ed372c3857e
Hang <me@hangxu.me>
2026-03-14 23:15:03 +0000

    feat: update sw_rev

--- Application/settings.h
+++ Application/settings.h
@@ -5,7 +5,7 @@
 #ifndef ECATV2_AX58100_H750_UNIVERSAL_SETTINGS_H
 #define ECATV2_AX58100_H750_UNIVERSAL_SETTINGS_H
 
-#define SW_REV "007"
+#define SW_REV "8"
 
 // If no packet is received from the master within this time,
 // treat the connection as lost
